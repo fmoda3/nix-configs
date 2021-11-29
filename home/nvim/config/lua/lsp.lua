5e9fd69598434b17e7b8c5252a7fef809d4749d0
Frank Moda <fmoda3@mac.com>
2021-11-28 23:17:09 -0500

    Fix issue where completion would trigger on enter with nothing selected

--- home/nvim/config/lua/lsp.lua
+++ home/nvim/config/lua/lsp.lua
@@ -11,8 +11,6 @@ util.colorize({
 })
 
 -- Autocompletion setup
-vim.o.completeopt = "menu,menuone,noselect"
-
 local has_words_before = function()
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
@@ -25,6 +23,9 @@ end
 local cmp = require'cmp'
 local lspkind = require('lspkind')
 cmp.setup({
+    completion = {
+	completeopt = 'menu,menuone,noselect,noinsert'
+    },
     snippet = {
 	expand = function(args)
 	    vim.fn["vsnip#anonymous"](args.body)
@@ -39,7 +40,7 @@ cmp.setup({
 	    i = cmp.mapping.abort(),
 	    c = cmp.mapping.close(),
 	}),
-	['<CR>'] = cmp.mapping.confirm({ select = true }),
+	['<CR>'] = cmp.mapping.confirm({ select = false }),
 	["<Tab>"] = cmp.mapping(function(fallback)
 	    if cmp.visible() then
 		cmp.select_next_item()
