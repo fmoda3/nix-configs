518edd8de55fcb0b60fa47882f77c30f24da0753
Frank Moda <frank@toasttab.com>
2022-02-08 18:30:38 -0500

    Switch typescript formatter to eslint_d

--- home/nvim/config/lua/lsp.lua
+++ home/nvim/config/lua/lsp.lua
@@ -197,7 +197,7 @@ null_ls.setup({
     sources = {
         null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.code_actions.eslint_d,
-        null_ls.builtins.formatting.prettier
+        null_ls.builtins.formatting.eslint_d
     },
     on_attach = on_attach
 })
