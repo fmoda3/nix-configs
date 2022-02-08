5ff965899676b2b2db777cdd3dd78319079a4084
Frank Moda <frank@toasttab.com>
2022-02-08 18:33:39 -0500

    Update grammars builder

--- pkgs/nvim-ts-grammars/default.nix
+++ pkgs/nvim-ts-grammars/default.nix
@@ -2,33 +2,22 @@
 
 let
   fetchGrammar = (v: fetchgit { inherit (v) url rev sha256 fetchSubmodules; });
-  builtGrammars = let
-    buildGrammar = name: grammar:
-      callPackage ./grammar.nix { } {
-        language = name;
-        version = tree-sitter.version;
-        source = fetchGrammar grammar;
-        location = if grammar ? location then grammar.location else null;
-      };
-
-    grammars' = (import ./grammars);
-    grammars = grammars' // {
-      tree-sitter-ocaml = grammars'.tree-sitter-ocaml // {
-        location = "ocaml";
-      };
-    } // {
-      tree-sitter-ocaml_interface = grammars'.tree-sitter-ocaml // {
-        location = "interface";
-      };
-    } // {
-      tree-sitter-typescript = grammars'.tree-sitter-typescript // {
-        location = "typescript";
-      };
-    } // {
-      tree-sitter-tsx = grammars'.tree-sitter-typescript // {
-        location = "tsx";
-      };
-    };
-  in lib.mapAttrs buildGrammar grammars;
+  builtGrammars =
+    let
+      change = name: grammar:
+        callPackage ./grammar.nix { } {
+          language = if grammar ? language then grammar.language else name;
+          version = tree-sitter.version;
+          source = fetchGrammar grammar;
+          location = if grammar ? location then grammar.location else null;
+        };
+      grammars' = (import ./grammars);
+      grammars = grammars' //
+        { tree-sitter-ocaml = grammars'.tree-sitter-ocaml // { location = "ocaml"; }; } //
+        { tree-sitter-ocaml-interface = grammars'.tree-sitter-ocaml // { location = "interface"; }; } //
+        { tree-sitter-typescript = grammars'.tree-sitter-typescript // { location = "typescript"; }; } //
+        { tree-sitter-tsx = grammars'.tree-sitter-typescript // { location = "tsx"; }; };
+    in
+    lib.mapAttrs change grammars;
   allGrammars = builtins.attrValues builtGrammars;
 in { inherit builtGrammars allGrammars; }
