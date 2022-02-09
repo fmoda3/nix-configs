262b4cc627e0353c8e48b2a1d482c52cf07fa2c0
Frank Moda <frank@toasttab.com>
2022-02-08 19:00:58 -0500

    Better package oktoast dependencies

--- home/work.nix
+++ home/work.nix
@@ -6,11 +6,7 @@
 
   home = {
     packages = [
-      pkgs.saml2aws # oktoast dep
-      pkgs.gnused # oktoast dep
-      # gnused installs "sed", but oktoast needs "gsed"
-      (pkgs.writeShellScriptBin "gsed" "exec -a $0 ${pkgs.gnused}/bin/sed \"$@\"")
-      pkgs.awscli # oktoast dep
+      pkgs.awscli
       pkgs.oktoast
     ];
   };
--- pkgs/oktoast/default.nix
+++ pkgs/oktoast/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, pkgs }:
+{ stdenv, fetchFromGitHub, pkgs, lib }:
 stdenv.mkDerivation {
   pname = "oktoast";
   version = "1.0";
@@ -8,8 +8,22 @@ stdenv.mkDerivation {
     rev = "9c11ea8337143a597d088d4a75a10424d82f45ee";
   };
 
+  nativeBuildInputs = [
+    pkgs.makeWrapper
+  ];
+
   installPhase = ''
     mkdir -p $out/bin
     cp oktoast $out/bin
+  '';
+
+  postFixup = ''
+    wrapProgram $out/bin/oktoast \
+      --prefix PATH : ${lib.makeBinPath [
+        pkgs.saml2aws
+        # gnused installs "sed", but oktoast needs "gsed"
+        (pkgs.writeShellScriptBin "gsed" "exec -a $0 ${pkgs.gnused}/bin/sed \"$@\"")
+        pkgs.awscli
+      ]}
   '';
 }
