d8fccad86924aa20466c2f96bd1e7ad4ccd2ca35
Frank Moda <frank@toasttab.com>
2022-02-10 00:04:53 -0500

    Adding movement keybinds

--- home/nvim/config/lua/settings.lua
+++ home/nvim/config/lua/settings.lua
@@ -21,3 +21,9 @@ opt.number = true
 vim.cmd("syntax enable")
 vim.cmd("colorscheme nord")
 opt.background="dark"
+
+-- Movement keybinds
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-hl', '<C-w>l', { noremap = true })
