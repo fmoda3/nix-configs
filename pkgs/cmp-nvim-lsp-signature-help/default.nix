7ad825b6f8a2f9fe9df02fcabc89c1df06d6f583
Frank Moda <frank@toasttab.com>
2022-02-24 00:29:26 -0500

    Update overridden packages

--- pkgs/cmp-nvim-lsp-signature-help/default.nix
+++ pkgs/cmp-nvim-lsp-signature-help/default.nix
@@ -1,12 +1,12 @@
 { fetchFromGitHub, vimUtils, ... }:
 vimUtils.buildVimPluginFrom2Nix {
     pname = "cmp-nvim-lsp-signature-help";
-    version = "2022-02-09";
+    version = "2022-02-15";
     src = fetchFromGitHub {
       owner = "hrsh7th";
       repo = "cmp-nvim-lsp-signature-help";
-      rev = "3f486a300c7f7296719c0705117afe24e3985766";
-      sha256 = "0hy11yb2ym9yk8ixs3951qk0qa8w0gzsqcm9qrjpvhk55ygpzzxs";
+      rev = "414619286928901600cf5b5ccb2f62666f82d3bd";
+      sha256 = "0ly41w9x7ygi0ii8i9k56g5p2sdxn345rf21rb7asnlck1ahc0r1";
     };
     meta.homepage = "https://github.com/hrsh7th/cmp-nvim-lsp-signature-help";
   }
--- pkgs/legendary-nvim/default.nix
+++ pkgs/legendary-nvim/default.nix
@@ -1,12 +1,12 @@
 { fetchFromGitHub, vimUtils, ... }:
 vimUtils.buildVimPluginFrom2Nix {
     pname = "legendary-nvim";
-    version = "2022-02-09";
+    version = "2022-02-16";
     src = fetchFromGitHub {
       owner = "mrjones2014";
       repo = "legendary.nvim";
-      rev = "c2ef7d38ca2fd14e70644faf1ccf6c6021ac5c81";
-      sha256 = "0z0g5b0da7pjn0w2jfsw1y1majjm9sxybg1kbm2mqxqp22zcibji";
+      rev = "8be2a9fc80a9ddc33d2d34b82161c8a4a04a82cd";
+      sha256 = "15h9l5q7bidd0knwcyl3ny8rrlplwykazdg44l335d6607czlccp";
     };
     meta.homepage = "https://github.com/mrjones2014/legendary.nvim";
   }
