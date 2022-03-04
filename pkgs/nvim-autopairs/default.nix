29da1bcf68b5f7c2e99dfdbed031d9074e0d7d71
Frank Moda <fmoda3@mac.com>
2022-03-04 13:05:12 -0500

    Switch back to main autopairs repo since fix is merged

--- pkgs/nvim-autopairs/default.nix
+++ pkgs/nvim-autopairs/default.nix
@@ -1,12 +1,12 @@
 { fetchFromGitHub, vimUtils, ... }:
 vimUtils.buildVimPluginFrom2Nix {
     pname = "nvim-autopairs";
-    version = "2022-03-03";
+    version = "2022-03-04";
     src = fetchFromGitHub {
-      owner = "fmoda3";
+      owner = "windwp";
       repo = "nvim-autopairs";
-      rev = "4fc3c19d1c8cf5cf76d374fabce621fc35bcce02";
-      sha256 = "0llfsq6qq3ylqmwxp30zibc1dmwm8g45q01wfwx6ix3dx4vb707r";
+      rev = "7bc61885cca93958a5e6de4873f4b930e87c6f9f";
+      sha256 = "0fkpf2r3plmgzxkzglxgrw6gq14ak4kyvwmp1kcdwgrqb3d0nf8p";
     };
-    meta.homepage = "https://github.com/fmoda3/nvim-autopairs";
+    meta.homepage = "https://github.com/windwp/nvim-autopairs";
   }
