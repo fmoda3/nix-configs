79d6b0395107e37ecafb83e6206044a2acea4669
Frank Moda <fmoda3@mac.com>
2022-03-04 13:21:33 -0500

    Fixed trouble description

--- home/nvim/config/lua/trouble-config.lua
+++ home/nvim/config/lua/trouble-config.lua
@@ -5,7 +5,7 @@ require('legendary').bind_keymaps({
     { '<Leader>xx', '<cmd>TroubleToggle<cr>', description = 'Trouble: Toggle', opts = opts },
 	{ '<Leader>xs', '<cmd>Trouble<cr>', description = 'Trouble: Open', opts = opts },
     { '<Leader>xc', '<cmd>TroubleClose<cr>', description = 'Trouble: Close', opts = opts },
-    { '<Leader>xr', '<cmd>TroubleRefresh<cr>', description = 'Trouble: Close', opts = opts },
+    { '<Leader>xr', '<cmd>TroubleRefresh<cr>', description = 'Trouble: Refresh', opts = opts },
 	{ '<Leader>xw', '<cmd>Trouble workspace_diagnostics<cr>', description = 'Trouble: Workspace diagnostics', opts = opts },
 	{ '<Leader>xd', '<cmd>Trouble document_diagnostics<cr>', description = 'Trouble: Document diagnostics', opts = opts },
 	{ '<Leader>xl', '<cmd>Trouble loclist<cr>', description = 'Trouble: Loclist diagnostics', opts = opts },
