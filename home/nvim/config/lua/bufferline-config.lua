04c1d3b6db836918e6fc59fa0795ea62ebc28029
Frank Moda <frank@toasttab.com>
2022-03-04 23:50:13 -0500

    Fix bufferline commands

--- home/nvim/config/lua/bufferline-config.lua
+++ home/nvim/config/lua/bufferline-config.lua
@@ -11,6 +11,6 @@ local opts = { silent = true, noremap = true }
 require('legendary').bind_keymaps({
     { '[b', '<cmd>BufferLineCycleNext<cr>', description = 'BufferLine: Next buffer', opts = opts },
     { ']b', '<cmd>BufferLineCyclePrev<cr>', description = 'BufferLine: Previous buffer', opts = opts },
-    { '<Leader>be', '<cmd>TroubleClose<cr>', description = 'BufferLine: Organize by extension', opts = opts },
-    { '<Leader>bd', '<cmd>TroubleRefresh<cr>', description = 'BufferLine: Organize by directory', opts = opts }
+    { '<Leader>be', '<cmd>BufferLineSortByExtension<cr>', description = 'BufferLine: Organize by extension', opts = opts },
+    { '<Leader>bd', '<cmd>BufferLineSortByDirectory<cr>', description = 'BufferLine: Organize by directory', opts = opts }
 })
