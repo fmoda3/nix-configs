7014d7e2ff8f624790be275c2a12b4d0fe24dbe3
Frank Moda <frank@toasttab.com>
2022-03-06 11:24:09 -0500

    Update autopairs

--- pkgs/nvim-autopairs/default.nix
+++ pkgs/nvim-autopairs/default.nix
@@ -1,12 +1,12 @@
 { fetchFromGitHub, vimUtils, ... }:
 vimUtils.buildVimPluginFrom2Nix {
     pname = "nvim-autopairs";
-    version = "2022-03-04";
+    version = "2022-03-06";
     src = fetchFromGitHub {
       owner = "windwp";
       repo = "nvim-autopairs";
-      rev = "7bc61885cca93958a5e6de4873f4b930e87c6f9f";
-      sha256 = "0fkpf2r3plmgzxkzglxgrw6gq14ak4kyvwmp1kcdwgrqb3d0nf8p";
+      rev = "d2cde7c5802b34d6391a8a3555a3b7b56482f2aa";
+      sha256 = "1cbijlqblxf5chc35lq8jl8pzfkx0a72bbf6bzz90nvrcn4xc6m3";
     };
     meta.homepage = "https://github.com/windwp/nvim-autopairs";
   }
