fdfffd9de1b1bff1c7c60c4ed6c0b18b4b812471
Frank Moda <frank@toasttab.com>
2022-03-08 00:20:24 -0500

    Add example haskell flake template

---
+++ flake-templates/haskell/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Example Haskell Project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, flake-compat }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        devShell = pkgs.haskellPackages.shellFor {
+          packages = p: [ ];
+
+          buildInputs = with pkgs.haskellPackages; [ cabal-install ghcid ];
+
+          withHoogle = true;
+        };
+      });
+}
