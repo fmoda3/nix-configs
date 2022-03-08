a33a2154c24e52a0512e5469fd8dc2dfa5d80a2c
Frank Moda <frank@toasttab.com>
2022-03-07 23:52:05 -0500

    Add javascript/typescript flake template

---
+++ flake-templates/javascript/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Example JavaScript/TypeScript project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, flake-compat }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs;
+            [
+              yarn
+              # Can override node version
+              # (yarn.override { nodejs = nodejs-12_x; })
+            ];
+        };
+      });
+}
