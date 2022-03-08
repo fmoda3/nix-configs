f10065c9f74cc3891d2ce9797e722c60a53df966
Frank Moda <frank@toasttab.com>
2022-03-07 23:10:43 -0500

    Update python flake template

--- nix-develop-examples/python/flake.nix
+++ nix-develop-examples/python/flake.nix
@@ -1,38 +1,22 @@
 {
-   description = "Example Python Project";
+  description = "Example Python Project";
 
-   inputs = {
-     nixpkgs.url = "github:NixOS/nixpkgs/master";
-     flake-utils.url = "github:numtide/flake-utils";
-     mach-nix.url = "github:DavHau/mach-nix?ref=3.3.0";
-   };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    mach-nix.url = "github:DavHau/mach-nix?ref=3.4.0";
+  };
 
-   outputs = { self, nixpkgs, flake-utils, mach-nix }:
-     let
-       # Customize starts
-       python = "python39";
-       devShell = pkgs:
-         pkgs.mkShell {
-           buildInputs = [
-             (pkgs.${python}.withPackages
-               (ps: with ps; [ pip black pyflakes isort ]))
-             pkgs.pyright
-           ];
-         };
-       # Customize ends
-     in flake-utils.lib.eachDefaultSystem (system:
-       let
-         pkgs = nixpkgs.legacyPackages.${system};
-         # https://github.com/DavHau/mach-nix/issues/153#issuecomment-717690154
-         mach-nix-wrapper = import mach-nix { inherit pkgs python; };
-         requirements = builtins.readFile ./requirements.txt;
-         pythonShell = mach-nix-wrapper.mkPythonShell { inherit requirements; };
-         mergeEnvs = envs:
-           pkgs.mkShell (builtins.foldl' (a: v: {
-             # runtime
-             buildInputs = a.buildInputs ++ v.buildInputs;
-             # build time
-             nativeBuildInputs = a.nativeBuildInputs ++ v.nativeBuildInputs;
-           }) (pkgs.mkShell { }) envs);
-       in { devShell = mergeEnvs [ (devShell pkgs) pythonShell ]; });
- }
+  outputs = { self, nixpkgs, flake-utils, flake-compat, mach-nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        mach-nix-wrapper = import mach-nix { inherit pkgs; };
+        requirements = builtins.readFile ./requirements.txt;
+        pythonShell = mach-nix-wrapper.mkPythonShell { inherit requirements; };
+      in { devShell = pythonShell; });
+}
