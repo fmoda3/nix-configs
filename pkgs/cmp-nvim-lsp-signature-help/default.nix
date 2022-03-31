c64bc2e1d1ca767feb2379034f40f143021474f1
Frank Moda <fmoda3@mac.com>
2022-03-30 21:55:18 -0400

    Update lsp signature help

--- pkgs/cmp-nvim-lsp-signature-help/default.nix
+++ pkgs/cmp-nvim-lsp-signature-help/default.nix
@@ -1,12 +1,12 @@
 { fetchFromGitHub, vimUtils, ... }:
 vimUtils.buildVimPluginFrom2Nix {
     pname = "cmp-nvim-lsp-signature-help";
-    version = "2022-02-15";
+    version = "2022-03-29";
     src = fetchFromGitHub {
       owner = "hrsh7th";
       repo = "cmp-nvim-lsp-signature-help";
-      rev = "7d6473ee5379a659e10abef4f5766a87ca41ec96";
-      sha256 = "1rc6bay445iygwyf2yj5nh2rk355yjvasb430pvmqfixgh1mf5in";
+      rev = "8014f6d120f72fe0a135025c4d41e3fe41fd411b";
+      sha256 = "1k61aw9mp012h625jqrf311vnsm2rg27k08lxa4nv8kp6nk7il29";
     };
     meta.homepage = "https://github.com/hrsh7th/cmp-nvim-lsp-signature-help";
   }
