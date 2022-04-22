ff57938ddf9819403314e1de2cb3786f8d0a10fb
Frank Moda <fmoda3@mac.com>
2022-04-22 13:57:34 -0400

    Rollback sumneko-lua-language-server until its fixed in 3.1.0

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -8,6 +8,7 @@ final: prev:
     jsons = prev.callPackage ./jsons { };
     typish = prev.callPackage ./typish { };
   };
+  sumneko-lua-language-server = prev.callPackage ./sumneko-lua-language-server { };
   toast-services = prev.callPackage ./toast-services { };
   vimPlugins = prev.vimPlugins // {
     cmp-nvim-lsp-signature-help = prev.callPackage ./cmp-nvim-lsp-signature-help { };
