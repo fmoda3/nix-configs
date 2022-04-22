076f43d3c71a144f7854ab6069485532898fa4c7
Frank Moda <fmoda3@mac.com>
2022-04-21 21:57:56 -0400

    Add borders to nvim completions

--- home/nvim/config/lua/cmp-config.lua
+++ home/nvim/config/lua/cmp-config.lua
@@ -20,6 +20,10 @@ cmp.setup({
             require('luasnip').lsp_expand(args.body)
         end,
     },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
     mapping = {
         ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
         ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
