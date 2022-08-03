11303deae90c84dec0e2ef433d300cfa154b1088
Frank Moda <frank@toasttab.com>
2022-08-03 11:46:35 -0400

    Update toast-services

--- pkgs/toast-services/default.nix
+++ pkgs/toast-services/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation {
 
   src = fetchGit {
     url = "git@github.com:toasttab/toast-services.git";
-    rev = "2f002293fdab9d4b4473b7a80cfa93b488deef13";
+    rev = "df47cd6619c5f2bc2918b8ba93425c2fd9f80da4";
     ref = "development";
   };
 
@@ -16,21 +16,23 @@ stdenv.mkDerivation {
   installPhase = ''
     mkdir -p $out/bin
     mkdir -p $out/bin/lib
-    cp deploy_g2_service.sh $out/bin/deploy_g2_service
-    cp deploy_spa.sh $out/bin/deploy_spa
-    cp destroy_g2_service.sh $out/bin/destroy_g2_service
-    cp destroy_spa.sh $out/bin/destroy_spa
+    cp deploy_g2_service.sh $out/bin/deploy_g2_service.sh
+    cp deploy_spa.sh $out/bin/deploy_spa.sh
+    cp destroy_g2_service.sh $out/bin/destroy_g2_service.sh
+    cp destroy_spa.sh $out/bin/destroy_spa.sh
     cp lib/check_aws_env.bash $out/bin/lib/check_aws_env.bash
+    cp lib/countdown.bash $out/bin/lib/countdown.bash
+    cp lib/codebuild_libs.bash $out/bin/lib/codebuild_libs.bash
   '';
 
   postFixup = ''
-    wrapProgram $out/bin/deploy_g2_service \
-      --prefix PATH : ${lib.makeBinPath [ pkgs.awscli ]}
-    wrapProgram $out/bin/deploy_spa \
-      --prefix PATH : ${lib.makeBinPath [ pkgs.awscli ]}
-    wrapProgram $out/bin/destroy_g2_service \
-      --prefix PATH : ${lib.makeBinPath [ pkgs.awscli ]}
-    wrapProgram $out/bin/destroy_spa \
-      --prefix PATH : ${lib.makeBinPath [ pkgs.awscli ]}
+    wrapProgram $out/bin/deploy_g2_service.sh \
+      --prefix PATH : ${lib.makeBinPath [ pkgs.awscli2 pkgs.jq ]}
+    wrapProgram $out/bin/deploy_spa.sh \
+      --prefix PATH : ${lib.makeBinPath [ pkgs.awscli2 pkgs.jq ]}
+    wrapProgram $out/bin/destroy_g2_service.sh \
+      --prefix PATH : ${lib.makeBinPath [ pkgs.awscli2 pkgs.jq ]}
+    wrapProgram $out/bin/destroy_spa.sh \
+      --prefix PATH : ${lib.makeBinPath [ pkgs.awscli2 pkgs.jq ]}
   '';
 }
