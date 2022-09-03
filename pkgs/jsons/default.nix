a6b31627a75c3fe9b33464f5e57ddf48fee7b44d
Frank Moda <frank@toasttab.com>
2022-09-03 16:34:07 -0400

    Fix jsons rev

--- pkgs/jsons/default.nix
+++ pkgs/jsons/default.nix
@@ -7,7 +7,7 @@ pkgs.python3Packages.buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "ramonhagenaars";
     repo = "jsons";
-    rev = "0sdwc57f3lwzhbcapjdbay9f8rn65rlspxa67a2i5apcgg403qpc";
+    rev = "9abbf3a3bd32435ac74bc98c3554ad3c71086036";
     sha256 = "0sdwc57f3lwzhbcapjdbay9f8rn65rlspxa67a2i5apcgg403qpc";
   };
 
