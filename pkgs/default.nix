1fadaf5a921a5ab349e197985478bdecc95964e8
Frank Moda <frank@toasttab.com>
2022-09-12 11:42:27 -0400

    Update flake

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,9 +5,9 @@ final: prev:
   nvim-ts-grammars = prev.callPackage ./nvim-ts-grammars { };
   oktoast = prev.callPackage ./oktoast { };
   pizzabox = prev.callPackage ./pizzabox { };
-  python3Packages = prev.python3Packages // {
-    jsons = prev.callPackage ./jsons { };
-  };
+  # python3Packages = prev.python3Packages // {
+  #   jsons = prev.callPackage ./jsons { };
+  # };
   toast-services = prev.callPackage ./toast-services { };
   # vimPlugins = prev.vimPlugins // {
   #   gitsigns-nvim = prev.callPackage ./gitsigns-nvim { };
