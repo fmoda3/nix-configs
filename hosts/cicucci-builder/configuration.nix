abbb6744124552cbaacfc5be52f6fdf717bf34d5
Frank Moda <fmoda3@mac.com>
2023-04-13 21:16:47 -0400

    Add authorized key to builder

--- hosts/cicucci-builder/configuration.nix
+++ hosts/cicucci-builder/configuration.nix
@@ -30,6 +30,10 @@
 
   security.sudo.wheelNeedsPassword = false;
 
+  users.users."root".openssh.authorizedKeys.keys = [
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCwu5RO/BMeiQnBtQo0sN+qOBK6v24cQbHfpr0H06NreWOMS3345I7ST5ncDrZlSCRluarpR95Bo1d0QGF1RjBWITUCAVgGX04IqywmlsGkiZVDtJjmOElxIkgFPnOAjXJ977WGVbgRxC5e5Z88Byp4zymWJ0kvF9sdhNL3VoXfYPrO2rFzQGxTcGDKcrmgEkn/8kmq4S+4vrJ0hmukAON63RkTvbC/P4eH23HcHC7sM+pm4hbsL7baxbNRp7kdf++2h4S0FbJ2snuH2ocHE/c4AAv4bOA3nFrmLckP5fsMIMAEP4mluPqyrQjJvDgdCpJfzmv8oZ4Q2TmjS9Civqh/4NI4Ptmse4jOp9SlyM0OEReXS/RBlXeg7t/Yft7Gv47bRwY65TzeRuu8Db5jUCruy36BJgHc74ybxqLak8+XcM83hMdPJtXalYYyAKfwermfM5uGwjuvrfrK9Nh/bVa0DmOk/rZq4veoQyHSf4xttAAg4VPEXjMVA6WfD8+r3aE="
+  ];
+
   i18n.defaultLocale = "en_US.UTF-8";
 
   # Disable the default module and import our override. We have
