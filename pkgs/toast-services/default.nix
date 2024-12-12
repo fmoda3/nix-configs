f9657663f7afdce12befb7df0b8f1985e37ea5d5
Frank Moda <frank@toasttab.com>
2024-12-12 15:03:02 -0500

    Update toast-services

--- pkgs/toast-services/default.nix
+++ pkgs/toast-services/default.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation {
 
   src = fetchGit {
     url = "git@github.toasttab.com:toasttab/toast-services.git";
-    rev = "8b9e7dc2d3f856b2c8668b92c07bd30b983e1bb5";
+    rev = "cdaa2d9e5d9961215490f67f3962708a563a2e7c";
     ref = "development";
   };
 
@@ -22,9 +22,7 @@ stdenv.mkDerivation {
     mkdir -p $out/bin
     mkdir -p $out/bin/lib
     cp deploy_g2_service.sh $out/bin/deploy_g2_service.sh
-    cp deploy_spa.sh $out/bin/deploy_spa.sh
     cp destroy_g2_service.sh $out/bin/destroy_g2_service.sh
-    cp destroy_spa.sh $out/bin/destroy_spa.sh
     cp lib/check_aws_env.bash $out/bin/lib/check_aws_env.bash
     cp lib/countdown.bash $out/bin/lib/countdown.bash
     cp lib/aws_libs.bash $out/bin/lib/aws_libs.bash
@@ -33,11 +31,7 @@ stdenv.mkDerivation {
   postFixup = ''
     wrapProgram $out/bin/deploy_g2_service.sh \
       --prefix PATH : ${lib.makeBinPath [ awscli2 jq ]}
-    wrapProgram $out/bin/deploy_spa.sh \
-      --prefix PATH : ${lib.makeBinPath [ awscli2 jq ]}
     wrapProgram $out/bin/destroy_g2_service.sh \
-      --prefix PATH : ${lib.makeBinPath [ awscli2 jq ]}
-    wrapProgram $out/bin/destroy_spa.sh \
       --prefix PATH : ${lib.makeBinPath [ awscli2 jq ]}
   '';
 }
