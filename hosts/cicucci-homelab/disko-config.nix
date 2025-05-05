657340476e0823c8f8672d40a8c80ca916731745
Frank Moda <fmoda3@mac.com>
2025-05-04 23:14:21 -0400

    Update disko for homelab

--- hosts/cicucci-homelab/disko-config.nix
+++ hosts/cicucci-homelab/disko-config.nix
@@ -5,43 +5,36 @@ _: {
         type = "disk";
         device = "/dev/nvme0n1";
         content = {
-          type = "table";
-          format = "gpt";
-          partitions = [
-            {
-              name = "ESP";
-              start = "1MiB";
-              end = "512MiB";
-              fs-type = "fat32";
-              bootable = true;
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "512MiB";
+              type = "EF00";
+              priority = 1;
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
               };
-            }
-            {
-              name = "nixos";
-              start = "512MiB";
+            };
+            nixos = {
               end = "-18GiB";
-              part-type = "primary";
+              priority = 2;
               content = {
                 type = "filesystem";
                 format = "ext4";
                 mountpoint = "/";
               };
-            }
-            {
-              name = "swap";
-              start = "-18GiB";
-              end = "100%";
-              part-type = "primary";
-              fs-type = "linux-swap";
+            };
+            swap = {
+              size = "100%";
+              priority = 3;
               content = {
                 type = "swap";
               };
-            }
-          ];
+            };
+          };
         };
       };
     };
