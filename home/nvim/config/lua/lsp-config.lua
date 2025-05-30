039d73e74bb10c721533f929ddf00b5b0ffeea0d
Frank Moda <fmoda3@mac.com>
2025-05-30 00:04:21 -0400

    Fix deprecated usages

--- home/nvim/config/lua/lsp-config.lua
+++ home/nvim/config/lua/lsp-config.lua
@@ -3,13 +3,6 @@ local nvim_lsp = require("lspconfig")
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-	local function buf_set_option(...)
-		vim.api.nvim_buf_set_option(bufnr, ...)
-	end
-
-	-- Enable completion triggered by <c-x><c-o>
-	buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
-
 	require("illuminate").on_attach(client)
 
 	-- Mappings.
@@ -37,7 +30,7 @@ local on_attach = function(client, bufnr)
 		{
 			"<space>e",
 			function()
-				vim.diagnostic.open_float(0, { scope = "line" })
+				vim.diagnostic.open_float({ scope = "line" })
 			end,
 			description = "Diagnostics: Show window",
 			opts = opts,
@@ -45,7 +38,7 @@ local on_attach = function(client, bufnr)
 		{
 			"[d",
 			function()
-				vim.diagnostic.goto_prev({ float = { border = "single" } })
+				vim.diagnostic.jump({ count = -1, float = { border = "single" } })
 			end,
 			description = "Diagnostics: Previous",
 			opts = opts,
@@ -53,7 +46,7 @@ local on_attach = function(client, bufnr)
 		{
 			"]d",
 			function()
-				vim.diagnostic.goto_next({ float = { border = "single" } })
+				vim.diagnostic.jump({ float = { border = "single" } })
 			end,
 			description = "Diagnostics: Next",
 			opts = opts,
