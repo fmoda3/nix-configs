3344518a2ee3d36a7492df5f283c40bdc0499491
Frank Moda <fmoda3@mac.com>
2025-05-31 19:09:24 -0400

    Update more homebridge formatting

--- pkgs/homebridge-config-ui-x/default.nix
+++ pkgs/homebridge-config-ui-x/default.nix
@@ -4,45 +4,42 @@
 , fetchFromGitHub
 , fetchNpmDeps
 , npmHooks
-, nodejs_22
 }:
 
-let
+buildNpmPackage (finalAttrs: {
+  pname = "homebridge-config-ui-x";
   version = "4.75.0";
+
   src = fetchFromGitHub {
     owner = "homebridge";
     repo = "homebridge-config-ui-x";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-zmr35ejcON5haUKkXiLBqAVSkP26QYVc3Ftnc76KnYw=";
   };
-
-  # Deps src and hash for ui subdirectory
-  npmDeps_ui = fetchNpmDeps {
-    name = "npm-deps-ui";
-    src = "${src}/ui";
-    hash = "sha256-WHGg2sINF1fH3tnuZsf+yjqoyWSPU5osOhuZb5VU+8I=";
-  };
-in
-buildNpmPackage {
-  pname = "homebridge-config-ui-x";
-  inherit version src;
-
-  nodejs = nodejs_22;
 
   # Deps hash for the root package
   npmDepsHash = "sha256-aqG9GQ2gdrCz+lxTOVYFo96jusb710KkguN/5R0Ye6c=";
 
   # Need to also run npm ci in the ui subdirectory
-  preBuild = ''
-    # Tricky way to run npmConfigHook multiple times
-    (
-      source ${npmHooks.npmConfigHook}/nix-support/setup-hook
-      npmRoot=ui npmDeps=${npmDeps_ui} makeCacheWritable= npmConfigHook
-    )
-    # Required to prevent "ng build" from failing due to
-    # prompting user for autocompletion
-    export CI=true
-  '';
+  preBuild =
+    let
+      # Deps src and hash for ui subdirectory
+      npmDeps_ui = fetchNpmDeps {
+        name = "npm-deps-ui";
+        src = "${finalAttrs.src}/ui";
+        hash = "sha256-WHGg2sINF1fH3tnuZsf+yjqoyWSPU5osOhuZb5VU+8I=";
+      };
+    in
+    ''
+      # Tricky way to run npmConfigHook multiple times
+      (
+        source ${npmHooks.npmConfigHook}/nix-support/setup-hook
+        npmRoot=ui npmDeps=${npmDeps_ui} makeCacheWritable= npmConfigHook
+      )
+      # Required to prevent "ng build" from failing due to
+      # prompting user for autocompletion
+      export CI=true
+    '';
 
   # On darwin, the build failed because openpty() is not declared
   # Uses the prebuild version of @homebridge/node-pty-prebuilt-multiarch instead
@@ -61,4 +58,4 @@ buildNpmPackage {
     platforms = lib.platforms.linux ++ lib.platforms.darwin;
     maintainers = with lib.maintainers; [ fmoda3 ];
   };
-}
+})
