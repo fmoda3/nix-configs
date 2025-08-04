115bbc6fb8fb2e2b0271634aa7e4fba56c075c19
Frank Moda <fmoda3@mac.com>
2025-08-04 19:50:56 -0400

    Update claudecode-nvim

--- pkgs/vimPlugins/claudecode-nvim/default.nix
+++ pkgs/vimPlugins/claudecode-nvim/default.nix
@@ -4,12 +4,12 @@
 }:
 vimUtils.buildVimPlugin {
   pname = "claudecode-nvim";
-  version = "2025-07-30";
+  version = "2025-08-04";
   src = fetchFromGitHub {
     owner = "coder";
     repo = "claudecode.nvim";
-    rev = "d0f97489d9064bdd55592106e99aa5f355a09914";
-    sha256 = "sha256-qmZPjZJ9UFxAWCY3NQwsu0nEniG/UasV/iCrG3S5tPQ=";
+    rev = "477009003cbec7e6088dbbeab46aba80f461d5f0";
+    sha256 = "sha256-P2FELIY8roeII4kVgk5BEHWkhelJCsaV6PyMIkEpC8I=";
   };
   dependencies = [ vimPlugins.snacks-nvim ];
   meta.homepage = "https://github.com/coder/claudecode.nvim/";
