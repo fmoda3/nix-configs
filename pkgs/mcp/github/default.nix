b389cb48ca2e990553c96ab2823b13a0acec62f6
Frank Moda <fmoda3@mac.com>
2025-08-10 22:13:54 -0400

    Update github-mcp-server

--- pkgs/mcp/github/default.nix
+++ pkgs/mcp/github/default.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   pname = "github-mcp-server";
-  version = "0.10.0";
+  version = "0.11.0";
 
   src = fetchFromGitHub {
     owner = "github";
     repo = "github-mcp-server";
     rev = "v${version}";
-    hash = "sha256-I7Y2vZdQllT8wVttf+axwvBF7Cv4gYM4vxw7qKEmhog=";
+    hash = "sha256-qgCysZMGyiuqBBvvbh5KmQ8tu0XhWcKihc9Z07Lw/kA=";
   };
 
-  vendorHash = "sha256-DeojCgMBwVclvoiEs462FoxIf3700XUjXvPbvRZE3CI=";
+  vendorHash = "sha256-+w3xsbzvTkNfnFxIM8k339soqFOUGETA+KMNABoUKFY=";
 
   subPackages = [ "cmd/github-mcp-server" ];
 
