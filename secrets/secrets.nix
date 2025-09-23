e1b4a0acf1de73e4319e2c343bc069f98a719322
Frank Moda <fmoda3@mac.com>
2025-09-23 13:21:32 -0400

    Update secrets to add cicucci-server

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -9,9 +9,10 @@ let
   #     ssh-keygen -t ed25519
   cicucci-desktop-fmoda3 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKKKgYlOjtFHLxJUqoQiqvoad6G1yNNXwI5/G1NIwu27";
   cicucci-laptop-fmoda3 = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCz3Wfp4pIEqpgegM4FCrEiehQEnwDXmTQmpL2+QUKerDQEep3bJYS2NKqHeMotuCgSjbhnObPPxpT7Vr8bq/NFdfLbuqFILx7mCCmzvWB3A050qQkwLuE00PEYmdsH25OhztgTMPvHMhtw1gNJCAyXtvI3DN0DCJKDRJFWOp/Kj70hxHNsQogPbBBgqzfF4j2aYC7X7h3bXHzOc4lKFhZ+7xIj0yGenSSw9PM9PUvEMlFL53uG4QsUisIOvs7IptXjOkdWAug/bRX+XpEFYmCEp+1H2UqZVRNmIV6Fa2FPXqRILuWOsCALmYV5oRk9MgPbufdJ9CHvmu1SMCJ/mlbt8jicmOvgdzdRBmYiWfjtZNh929pP8c9BgFCNt4efZ5qqynlUzywuO4agw1iOXi4KppPLbb7UisnMo9+NtEjHXtneRJnPgYi+KmNyS/7O1/9egDt64bB9+ZkGjajeOVELxcEvwrHupHeLb3N3gC7lkAoRwmGHWxHKcyDS6kUQRC0=";
+  cicucci-server-fmoda3 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN8fct8sqoXsqMJFaiYZxKxC4IPKDWXL+ZGfW387RNKo";
   cicucci-homelab-fmoda3 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMbsadR1/6wz//uJ7fjuQnyBd3shjFcaLl5wlhSaFD+4";
   work-laptop-fmoda3 = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCYl3aVZjHTsKEboUS7r1YYAPdb39SKiIz1VtIVTLARR1goxg34Zbq0drxYyToBD151UmQ8Rs8Ybv3WVYQISdp9j4B8ZVjijLdL1SyajMAOztz5/3vlbEU9+tZUnd9VxEi7bRB31uFpaAjCYghHDaY22S8UzRqg0xZfQc/MEa7rgbXP1gxEeolEOJYohSk6ko3X6yAW15vd8mmJCThbtYbLkp31Yhdbo0KnyBIQo7or8IK5qbjHFdMwveYh29QXur7fZ+bDvFWUvW6DnrBkvMSZAymDACiOYUOenqmmdIhzyC3QN2RosABF3tB2rBkPb4WhfTQbyx8mYSZLkl5mUuGhMD0wXhmmcaoRV3miT807D1lDe9+bxcXnZEQSzTpPUQMgm12F2LkWnF7eZDTHcVZRhYUxrepHFHl7pq2N50LVq5ThNF+KImc1yqb7X0646cHibn9TxB2lKN8miACdepGMyNYfNmuJVxyoHh9y2MNhUohFc6QwvaEUtjFgj1jJtW8=";
-  users = [ cicucci-desktop-fmoda3 cicucci-laptop-fmoda3 cicucci-homelab-fmoda3 work-laptop-fmoda3 ];
+  users = [ cicucci-desktop-fmoda3 cicucci-laptop-fmoda3 cicucci-server-fmoda3 cicucci-homelab-fmoda3 work-laptop-fmoda3 ];
 
   # get system's ssh public key by command:
   #    cat /etc/ssh/ssh_host_ed25519_key.pub
@@ -21,7 +22,7 @@ let
 
   all = users ++ systems;
   work = [ work-laptop-fmoda3 ];
-  personal = [ cicucci-desktop-fmoda3 cicucci-laptop-fmoda3 cicucci-homelab-fmoda3 cicucci-dns cicucci-homelab ];
+  personal = [ cicucci-desktop-fmoda3 cicucci-laptop-fmoda3 cicucci-server-fmoda3 cicucci-homelab-fmoda3 cicucci-dns cicucci-homelab ];
 in
 {
   "anthropic_ai_key.age".publicKeys = all;
