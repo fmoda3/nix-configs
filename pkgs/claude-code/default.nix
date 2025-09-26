5a71b364deb8185ca4591b8e2da0269499714c8e
Frank Moda <fmoda3@mac.com>
2025-09-25 22:57:00 -0400

    Update claude-code

--- pkgs/claude-code/default.nix
+++ pkgs/claude-code/default.nix
@@ -7,16 +7,16 @@
 
 buildNpmPackage rec {
   pname = "claude-code";
-  version = "1.0.125";
+  version = "1.0.126";
 
   nodejs = nodejs_20; # required for sandboxed Nix builds on Darwin
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-    hash = "sha256-ffKP7k88aUngGhK/CMwRhSRbpO2nCk64xvNWjW6q1rw=";
+    hash = "sha256-U6uYRkmVqMoJDAAzLHpF9G5OglPhLqPuwe6gWMQPx78=";
   };
 
-  npmDepsHash = "sha256-XetLFXZZFH/F1xEknyKezkcA4c1f8YpIDAtbQX/I7n4=";
+  npmDepsHash = "sha256-m+GYa3uPfkUDV+p95uQToY3n/k0JG8hbppBn0GUeV+8=";
 
   postPatch = ''
     cp ${./package-lock.json} package-lock.json
