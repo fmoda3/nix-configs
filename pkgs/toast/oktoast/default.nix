e47d9c3e42493bc114acdd6ff8ccae2f8e16e058
Frank Moda <frank@toasttab.com>
2025-10-01 10:20:35 -0400

    Update oktoast

--- pkgs/toast/oktoast/default.nix
+++ pkgs/toast/oktoast/default.nix
@@ -12,28 +12,12 @@ stdenv.mkDerivation {
 
   src = fetchGit {
     url = "git@github.toasttab.com:toasttab/oktoast-setup.git";
-    rev = "572d166d4d56732ed22ec2d11aee09e13e4ecb70";
+    rev = "ce9105ab084eb1c16288041dd1b09bc96f881364";
   };
 
   nativeBuildInputs = [
     makeWrapper
   ];
-
-  postPatch = ''
-    # Replace hardcoded aws_session_duration = 3600 with variable substitution for bedrock
-    # This works within heredoc blocks by using variable substitution
-    sed -i 's/aws_session_duration = 3600/aws_session_duration = $AWS_SESSION_DURATION/' oktoast
-
-    # Add logic after ENVIRONMENT=$1 to set the session duration based on environment
-    sed -i '/ENVIRONMENT=\$1/a\
-    \
-    # Set AWS session duration based on environment\
-    if [[ "$ENVIRONMENT" == "bedrock" ]]; then\
-      AWS_SESSION_DURATION=43200\
-    else\
-      AWS_SESSION_DURATION=3600\
-    fi' oktoast
-  '';
 
   installPhase = ''
     mkdir -p $out/bin
