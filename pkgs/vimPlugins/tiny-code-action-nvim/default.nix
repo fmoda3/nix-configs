9c154f56615d6a0b23935777392020649510ec8e
Frank Moda <fmoda3@mac.com>
2025-11-05 13:58:00 -0500

    Update tiny-code-action-nvim

--- pkgs/vimPlugins/tiny-code-action-nvim/default.nix
+++ pkgs/vimPlugins/tiny-code-action-nvim/default.nix
@@ -4,12 +4,12 @@
 }:
 vimUtils.buildVimPlugin {
   pname = "tiny-code-action-nvim";
-  version = "2025-11-04";
+  version = "2025-11-05";
   src = fetchFromGitHub {
     owner = "rachartier";
     repo = "tiny-code-action.nvim";
-    rev = "93f3aab43cd78a5f05ce1b2e866e029468b1f8f6";
-    sha256 = "sha256-93L4Vd4ehvX5Mb7iOgTYR5NWzl7hUBadbxsKuwQ8dy0=";
+    rev = "7aaa18eea993cc5796c6128702eaedc05e55f6e8";
+    sha256 = "sha256-orAR6p2L4CHuhRMstuLgS4cYKK5onKtuV5I6v0PZE3M=";
   };
   dependencies = [ vimPlugins.plenary-nvim ];
   nvimSkipModules = [
