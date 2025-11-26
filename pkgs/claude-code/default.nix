9e0e742a04d8c0853c428fccaa81463b6026a8eb
Frank Moda <fmoda3@mac.com>
2025-11-26 14:38:19 -0500

    Update claude-code

--- pkgs/claude-code/default.nix
+++ pkgs/claude-code/default.nix
@@ -8,14 +8,14 @@
 
 buildNpmPackage (finalAttrs: {
   pname = "claude-code";
-  version = "2.0.53";
+  version = "2.0.54";
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${finalAttrs.version}.tgz";
-    hash = "sha256-GDk3oROfwlreyZ95oWkUc/OAv8pRHhHLDcwmRzXq3Wg=";
+    hash = "sha256-EVZueeW1MewYmQSHp4flcShqHy5H0S4gET3XtK+ttQA=";
   };
 
-  npmDepsHash = "sha256-owrDXjUvqxnKxZ1iGgeIlWxzTDRdTSzgy+6DrKrOrQE=";
+  npmDepsHash = "sha256-q9gFtW90nfq+qhuk8bl+wfrO7D6fmc4dpaIp5zJ7z18=";
 
   postPatch = ''
     cp ${./package-lock.json} package-lock.json
