59ca5eb59ebd7fa6c4985dbfff774bc1692d4914
Frank Moda <fmoda3@mac.com>
2026-01-12 12:44:06 -0500

    For nvim-treesitter, use the nix installed list, instead of the supported list

--- home/nvim/config/lua/treesitter-config.lua
+++ home/nvim/config/lua/treesitter-config.lua
@@ -1,6 +1,19 @@
-local available_parsers = vim.tbl_keys(require("nvim-treesitter.parsers"))
+local parser_files = vim.api.nvim_get_runtime_file("parser/*", true)
+local installed_parsers = {}
+local seen_parsers = {}
 
-for _, parser in ipairs(available_parsers) do
+for _, parser_file in ipairs(parser_files) do
+	local parser = vim.fn.fnamemodify(parser_file, ":t:r")
+
+	if parser ~= "" and not seen_parsers[parser] then
+		seen_parsers[parser] = true
+		table.insert(installed_parsers, parser)
+	end
+end
+
+table.sort(installed_parsers)
+
+for _, parser in ipairs(installed_parsers) do
 	local filetypes = parser -- In this case, parser is the filetype/language name
 	vim.treesitter.language.register(parser, filetypes)
 
