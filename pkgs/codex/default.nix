dfac38155e58046c06322782470f34941928085d
Frank Moda <fmoda3@mac.com>
2026-02-22 10:14:20 -0500

    Update codex

--- pkgs/codex/default.nix
+++ pkgs/codex/default.nix
@@ -18,13 +18,13 @@
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "codex";
-  version = "0.101.0";
+  version = "0.104.0";
 
   src = fetchFromGitHub {
     owner = "openai";
     repo = "codex";
     tag = "rust-v${finalAttrs.version}";
-    hash = "sha256-m2Jq7fbSXQ/O3bNBr6zbnQERhk2FZXb+AlGZsHn8GuQ=";
+    hash = "sha256-spWb/msjl9am7E4UkZfEoH0diFbvAfydJKJQM1N1aoI=";
   };
 
   sourceRoot = "${finalAttrs.src.name}/codex-rs";
