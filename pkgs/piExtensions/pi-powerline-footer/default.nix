f20bf01722c9e1cfcfcde22cdcfab64480dc8298
Frank Moda <fmoda3@mac.com>
2026-02-28 10:07:08 -0500

    Update pi-powerline-footer

--- pkgs/piExtensions/pi-powerline-footer/default.nix
+++ pkgs/piExtensions/pi-powerline-footer/default.nix
@@ -8,8 +8,8 @@ stdenvNoCC.mkDerivation {
   src = fetchFromGitHub {
     owner = "nicobailon";
     repo = "pi-powerline-footer";
-    rev = "aed65b62e101f146532ae5156a30f9950f4d477d";
-    sha256 = "sha256-7PIWXW4lazsy0jPh2LpLnSvxVJcWI1LbY56Qh024s5E=";
+    rev = "ced10bcd14273c99637c2474840286c173acc1d7";
+    sha256 = "sha256-4nxY66Xx38RVhN1LUVMqpwwruwVpOCNhi+tGYRL5fkM=";
   };
 
   # fix crash until https://github.com/nicobailon/pi-powerline-footer/issues/4 is fixed
