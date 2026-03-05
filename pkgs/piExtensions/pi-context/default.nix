7cbbe5cfb6301785e844e2ccc9031538fdf295d4
Frank Moda <fmoda3@mac.com>
2026-03-05 11:29:57 -0500

    Update pi-context

--- pkgs/piExtensions/pi-context/default.nix
+++ pkgs/piExtensions/pi-context/default.nix
@@ -3,13 +3,13 @@
 }:
 stdenvNoCC.mkDerivation {
   pname = "pi-context";
-  version = "1.1.0";
+  version = "1.1.1";
 
   src = fetchFromGitHub {
     owner = "ttttmr";
     repo = "pi-context";
-    rev = "7fd8752d05d98e4e38a7d0bc22de51d72d99f398";
-    sha256 = "sha256-cbcXUSzZfAXupQ1fzsise4OXFwH1N2VAIA3jdDyLJ/U=";
+    rev = "a5062d1def8faec29eb84c4e9dc0b907452d479d";
+    sha256 = "sha256-MfmZ7ckGzWZFu+wTuVJhyTCYotBMIyGZhAZNozRjFq8=";
   };
 
   installPhase = ''
