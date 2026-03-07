ff93a58d43fde3c703dcb453a409fdbafd5b7ead
Frank Moda <fmoda3@mac.com>
2026-03-07 09:35:00 -0500

    Update pi-coding-agent

--- pkgs/pi-coding-agent/default.nix
+++ pkgs/pi-coding-agent/default.nix
@@ -13,13 +13,13 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "pi-coding-agent";
-  version = "0.56.3";
+  version = "0.57.0";
 
   src = fetchFromGitHub {
     owner = "badlogic";
     repo = "pi-mono";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-QLNzIw/NrZC3nUtESUVp8eo0sUHR8CmLNp+PZmvRTc4=";
+    hash = "sha256-fUTYAkPf5QrX2Mv2J25vOpbihCXRa7/v73tkcpQnzoI=";
   };
 
   node_modules = stdenvNoCC.mkDerivation {
@@ -58,7 +58,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
 
     dontFixup = true;
 
-    outputHash = "sha256-Zu4H0TVDk0umC2zYalvoVtS9x3yZvTaoW3r6MYNtZRM=";
+    outputHash = "sha256-newDyYupBVXL8rxH8SNFl85uM8DeQnplufHaM1NuBZw=";
     outputHashAlgo = "sha256";
     outputHashMode = "recursive";
   };
