efdfecf03ba68aca91eb1bb167163b321029c890
Frank Moda <fmoda3@mac.com>
2026-03-21 16:49:19 -0400

    Update homebridge-config-ui-x

--- pkgs/homebridge-config-ui-x/default.nix
+++ pkgs/homebridge-config-ui-x/default.nix
@@ -10,28 +10,49 @@
 ,
 }:
 
+let
+  # Fix broken lockfile: monaco-editor pins dompurify@3.2.7 but the npm override
+  # in package.json forces 3.3.3. Update the lockfile to match the override so
+  # monaco-editor uses the top-level dompurify@3.3.3 instead of a missing 3.2.7.
+  patchLockfile = lockfile: ''
+    ${lib.getExe python3} -c "
+    import json
+    lockfile = '${lockfile}'
+    with open(lockfile) as f:
+        data = json.load(f)
+    me = data['packages']['node_modules/monaco-editor']
+    me['dependencies']['dompurify'] = '3.3.3'
+    with open(lockfile, 'w') as f:
+        json.dump(data, f, indent=2)
+        f.write('\n')
+    "
+  '';
+in
+
 buildNpmPackage.override { nodejs = nodejs_22; } (finalAttrs: {
   pname = "homebridge-config-ui-x";
-  version = "5.19.0";
+  version = "5.20.0";
 
   src = fetchFromGitHub {
     owner = "homebridge";
     repo = "homebridge-config-ui-x";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-PJ+dpI4vuT1NbmbqDe5sLXWayCHu1LPjX/SgNxNpKjE=";
+    hash = "sha256-d3y8mPk/+b4MA7h9VpO3nn1DS3C/C2Vu9Yq3+0cDJno=";
   };
 
   # Deps hash for the root package
-  npmDepsHash = "sha256-lENiS4SDxESpHzQrq9uuBWQmdbOpfDls6FKqq/KCp9w=";
+  npmDepsHash = "sha256-cIvGrBaRL9njfRd6FkUtpQZhl09ac/c3f61WvqfoMmI=";
 
   # Deps src and hash for ui subdirectory
   npmDeps_ui = fetchNpmDeps {
     name = "npm-deps-ui";
     src = "${finalAttrs.src}/ui";
-    hash = "sha256-3Bpo8l0aPKbLr/LVVwYa+H4kijt7fyIcwk4icDmWmUQ=";
+    hash = "sha256-9vmrGsdgWCfYmfpBK3C2xeddKycIZr9tu8gEMiLmuSw=";
+    preBuild = patchLockfile "package-lock.json";
+    nativeBuildInputs = [ python3 ];
   };
 
-  # npmFlags = [ "--legacy-peer-deps" ];
+  postPatch = patchLockfile "ui/package-lock.json";
 
   # Need to also run npm ci in the ui subdirectory
   preBuild = ''
