12a03af1778bca7f4c465f8fd8b2a283a774c88f
Frank Moda <fmoda3@mac.com>
2026-03-29 12:54:38 -0400

    Update pi-ask-tool

--- pkgs/piExtensions/pi-ask-tool/default.nix
+++ pkgs/piExtensions/pi-ask-tool/default.nix
@@ -3,13 +3,13 @@
 }:
 stdenvNoCC.mkDerivation {
   pname = "pi-ask-tool";
-  version = "2026-03-23";
+  version = "2026-03-29";
 
   src = fetchFromGitHub {
     owner = "devkade";
     repo = "pi-ask-tool";
-    rev = "fde70011e75ca60baff37f335f824b6cf26c9fac";
-    sha256 = "sha256-q8Wsw1ysVMMMeDHsstIirbeb6ST2K8+dK5IG7ukKXFA=";
+    rev = "d4e2c0c1a28afd5440c43771a9efb8b5aa190529";
+    sha256 = "sha256-ETZ5OmD2n9Uq7Z0eJpC3FYSguiLiPINXUXsLKJCdTKY=";
   };
 
   installPhase = ''
