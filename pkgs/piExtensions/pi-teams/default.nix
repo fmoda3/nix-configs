5f3e144985f51f88dccca12a12b2b3fd83d59803
Frank Moda <fmoda3@mac.com>
2026-04-03 11:19:47 -0400

    Update pi-teams

--- pkgs/piExtensions/pi-teams/default.nix
+++ pkgs/piExtensions/pi-teams/default.nix
@@ -3,13 +3,13 @@
 }:
 stdenvNoCC.mkDerivation {
   pname = "pi-teams";
-  version = "2026-03-20";
+  version = "2026-04-03";
 
   src = fetchFromGitHub {
     owner = "burggraf";
     repo = "pi-teams";
-    rev = "24596c922e832262626ab199fb3b42b3c42298d2";
-    sha256 = "sha256-FHKHJ0o6zr+jhS5yyuoosmoWBZO4wRO0dHKOo3Hu9+k=";
+    rev = "965c02e2de9db77739f95766f5f66c86b4c272b0";
+    sha256 = "sha256-vvTwFLRPVufnp0gxWtZlbUxC/dnQtj4TzretuR3ThaM=";
   };
 
   installPhase = ''
