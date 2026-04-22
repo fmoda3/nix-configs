93c1a9b68d51c7b5fab73af619dbbee1a602fa1d
Frank Moda <fmoda3@mac.com>
2026-04-21 20:31:45 -0400

    Update pi-mcp-adapter

--- pkgs/piExtensions/pi-mcp-adapter/default.nix
+++ pkgs/piExtensions/pi-mcp-adapter/default.nix
@@ -3,16 +3,16 @@
 }:
 buildNpmPackage {
   pname = "pi-mcp-adapter";
-  version = "2026-04-13";
+  version = "2026-04-21";
 
   src = fetchFromGitHub {
     owner = "nicobailon";
     repo = "pi-mcp-adapter";
-    rev = "999c735f2eb6f8c9237ad9b76668a93964d8be5c";
-    sha256 = "sha256-41f4kS6At7GQIfStEeQPRIQaFN5oMs6SrgDsNTeHhLE=";
+    rev = "d351b6d134f2ff4ec6f35454927629e8b6ab316c";
+    sha256 = "sha256-s9XWte5wfMnD77Jx7CzRycl76M9SR82zb4Lio9XeO3M=";
   };
 
-  npmDepsHash = "sha256-9P71EDq++Bmez3QDEbOL+PCtCFI2ajxy345stBOBp8k=";
+  npmDepsHash = "sha256-p0UyUcK7S9BWQtuarEMUOfvE+UXwIj5IJWZFFg0FDWo=";
   dontNpmBuild = true;
 
   installPhase = ''
