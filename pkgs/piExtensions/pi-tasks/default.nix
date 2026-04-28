e532909c8072dc648dd8cfc8833a0f7c767d9871
Frank Moda <fmoda3@mac.com>
2026-04-28 10:11:38 -0400

    Update pi-tasks

--- pkgs/piExtensions/pi-tasks/default.nix
+++ pkgs/piExtensions/pi-tasks/default.nix
@@ -4,13 +4,13 @@
 
 buildPiExtension {
   pname = "pi-tasks";
-  version = "2026-03-24";
+  version = "2026-04-28";
 
   src = fetchFromGitHub {
     owner = "tintinweb";
     repo = "pi-tasks";
-    rev = "46cca7a734bbeafb6db771eb06cb02b55b26e42d";
-    sha256 = "sha256-tlRB2KcfX96n8XVm4cwgMcskJdA19WynR93ffVEGY88=";
+    rev = "d47a9acb861bede4c476c6e0fa50145940d35c5b";
+    sha256 = "sha256-7Fbap7TaoGZu9klMDsLXCQ4r5TkRAvNYRTAhNaIkIIg=";
   };
 
   npmDepsHash = "sha256-ng0q5Ml2hWPBV7cAnbqCRPukWCCC7WeANcEvyTYPO9c=";
