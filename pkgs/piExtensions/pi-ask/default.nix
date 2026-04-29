d25e166dd31360ab3df3a6577bbb84c95b55f579
Frank Moda <fmoda3@mac.com>
2026-04-29 10:48:22 -0400

    Update pi-ask

--- pkgs/piExtensions/pi-ask/default.nix
+++ pkgs/piExtensions/pi-ask/default.nix
@@ -4,13 +4,13 @@
 
 buildPiExtension {
   pname = "pi-ask";
-  version = "0.6.1-unstable-2026-04-28";
+  version = "0.6.1-unstable-2026-04-29";
 
   src = fetchFromGitHub {
     owner = "eko24ive";
     repo = "pi-ask";
-    rev = "a29992123cb5dbe78bdb19b7382f2bd392c1f35d";
-    sha256 = "sha256-u9kqxNW5KAYthoGruGCT+qTDZdJ6D1os/TliJCL1Z3E=";
+    rev = "4153f6b09f97ca0242e1a06309e90c9ff35ae31e";
+    sha256 = "sha256-8n9frJi5TxgbaGDK6lmQJbajpeVeO1lYPVJCjS4pNdU=";
   };
 
   prunePaths = [ ".github" ];
