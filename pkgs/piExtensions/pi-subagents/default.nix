14cbd0f82dfe3f5e48a5c5dceccdaa6a00613437
Frank Moda <fmoda3@mac.com>
2026-05-02 23:57:43 -0400

    Update pi-subagents

--- pkgs/piExtensions/pi-subagents/default.nix
+++ pkgs/piExtensions/pi-subagents/default.nix
@@ -9,8 +9,8 @@ buildPiExtension {
   src = fetchFromGitHub {
     owner = "nicobailon";
     repo = "pi-subagents";
-    rev = "1fd371d2a068458741a15507edc6cd49a9807486";
-    sha256 = "sha256-YW8Wvc7+3MeKvE4kqAG/cuVIudxWBWcn80qY/yR9Nys=";
+    rev = "820a5f1770ccf632b48b26ee5c0b4dd713ded074";
+    sha256 = "sha256-x7CU+QBw/ib05x5KO8v87P75N7N46NPmxpTE3thY5zw=";
   };
 
   prunePaths = [ ".github" ];
