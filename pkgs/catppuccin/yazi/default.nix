e01d49d5a9feae03da6186d45f5d4c755b62d407
Frank Moda <fmoda3@mac.com>
2026-05-14 23:08:21 -0400

    Update yazi

--- pkgs/catppuccin/yazi/default.nix
+++ pkgs/catppuccin/yazi/default.nix
@@ -3,12 +3,12 @@
 }:
 stdenvNoCC.mkDerivation {
   pname = "catppuccin-yazi";
-  version = "2025-12-29";
+  version = "2026-05-14";
 
   src = fetchFromGitHub {
     owner = "catppuccin";
     repo = "yazi";
-    rev = "fc69d6472d29b823c4980d23186c9c120a0ad32c";
+    rev = "41f24ed142e34109a9a65a5dfe58c1b4eb6d2fd9";
     sha256 = "sha256-Og33IGS9pTim6LEH33CO102wpGnPomiperFbqfgrJjw=";
   };
 
