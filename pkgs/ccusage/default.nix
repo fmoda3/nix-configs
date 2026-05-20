a6954a2f373a9de04d821157facd78d90d9bc50f
Frank Moda <fmoda3@mac.com>
2026-05-20 09:37:27 -0400

    Update ccusage

--- pkgs/ccusage/default.nix
+++ pkgs/ccusage/default.nix
@@ -5,14 +5,14 @@
 
 buildNpmPackage (finalAttrs: {
   pname = "ccusage";
-  version = "19.0.3";
+  version = "20.0.1";
 
   src = fetchzip {
     url = "https://registry.npmjs.org/ccusage/-/ccusage-${finalAttrs.version}.tgz";
-    hash = "sha256-9nIZhmt9h2pzEtCpKs4SJ+2T6I+w4lFcAnRGeXvbgxk=";
+    hash = "sha256-UHdOxQaVE080ThcnXaJ+DphQu46p6xsioqpXALF44gM=";
   };
 
-  npmDepsHash = "sha256-1Lvlt9F7hm8dleBiQ84oDl7xLk9a03uYkBRKx35vT2k=";
+  npmDepsHash = "sha256-8wcZEYMcW4/+pQtVpnTVG8GiqutjXSQWxqeX+M55IlQ=";
   forceEmptyCache = true;
 
   postPatch = ''
@@ -26,6 +26,11 @@ buildNpmPackage (finalAttrs: {
     
     mkdir -p $out/bin $out/lib/node_modules/ccusage
     cp -r . $out/lib/node_modules/ccusage/
+
+    # The JS launcher attempts to chmod the native optionalDependency at runtime
+    # if it is not executable. Make it executable during the build instead,
+    # since the Nix store is read-only at runtime.
+    find $out/lib/node_modules/ccusage/node_modules/@ccusage -path '*/bin/ccusage*' -type f -exec chmod +x {} \;
     
     # Create symlink for the binary
     ln -s $out/lib/node_modules/ccusage/dist/cli.js $out/bin/ccusage
