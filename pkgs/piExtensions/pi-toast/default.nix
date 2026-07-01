6d2b935e5186732379ff012398338f5736521665
Frank Moda <frank@toasttab.com>
2026-07-01 16:29:01 -0400

    Update pi-toast

--- pkgs/piExtensions/pi-toast/default.nix
+++ pkgs/piExtensions/pi-toast/default.nix
@@ -2,26 +2,26 @@
 
 buildPiExtension {
   pname = "pi-toast";
-  version = "2026-06-29";
+  version = "2026-07-01";
 
   src = fetchGit {
     url = "git@github.toasttab.com:toasttab/pi-toast.git";
-    rev = "0f103d1f8bcd7e8ce13842fea3a982fa417613f9";
-    narHash = "sha256-iqg5L2d/ZVHkhrbvdavsJjjr4D/Qrc0GCN5zOvbaxow=";
+    rev = "d2d0fb13349a83d9a28b71184c2d55ecce735af8";
+    narHash = "sha256-2VHMo7kkJzmP1GOrmYpzrUj7qw+6Il1XFiT07T1fnXs=";
   };
 
   postPatch = ''
     substituteInPlace package-lock.json \
-      --replace-fail $'"node_modules/@earendil-works/pi-coding-agent/node_modules/@earendil-works/pi-agent-core": {\n      "version": "0.80.1",\n      "resolved": "https://registry.npmjs.org/@earendil-works/pi-agent-core/-/pi-agent-core-0.80.1.tgz",\n      "license": "MIT",\n      "peer": true,' \
-                     $'"node_modules/@earendil-works/pi-coding-agent/node_modules/@earendil-works/pi-agent-core": {\n      "version": "0.80.1",\n      "resolved": "https://registry.npmjs.org/@earendil-works/pi-agent-core/-/pi-agent-core-0.80.1.tgz",\n      "integrity": "sha512-/Y4XzinEIQekXRjMxtYy2QJt6y43Gw+7itAvsA8TNCuUrYMBI6WvZjlMMcYsjK+Oyt919noN0EsT9/Xd3D0ziw==",\n      "license": "MIT",\n      "peer": true,' \
-      --replace-fail $'"node_modules/@earendil-works/pi-coding-agent/node_modules/@earendil-works/pi-ai": {\n      "version": "0.80.1",\n      "resolved": "https://registry.npmjs.org/@earendil-works/pi-ai/-/pi-ai-0.80.1.tgz",\n      "license": "MIT",\n      "peer": true,' \
-                     $'"node_modules/@earendil-works/pi-coding-agent/node_modules/@earendil-works/pi-ai": {\n      "version": "0.80.1",\n      "resolved": "https://registry.npmjs.org/@earendil-works/pi-ai/-/pi-ai-0.80.1.tgz",\n      "integrity": "sha512-qsHmuoBRu7a6rkOis/Elwl8jEhA9T884H/mXMP3tbz1slEBI+dHaY3zJ0nlEqUl+jcTz1FJNdgOVLG4sQHOPXQ==",\n      "license": "MIT",\n      "peer": true,' \
-      --replace-fail $'"node_modules/@earendil-works/pi-coding-agent/node_modules/@earendil-works/pi-tui": {\n      "version": "0.80.1",\n      "resolved": "https://registry.npmjs.org/@earendil-works/pi-tui/-/pi-tui-0.80.1.tgz",\n      "license": "MIT",\n      "peer": true,' \
-                     $'"node_modules/@earendil-works/pi-coding-agent/node_modules/@earendil-works/pi-tui": {\n      "version": "0.80.1",\n      "resolved": "https://registry.npmjs.org/@earendil-works/pi-tui/-/pi-tui-0.80.1.tgz",\n      "integrity": "sha512-lmXGOiFv5KMati4VfdQt1L/7/m4yiJ8Xqm96COgkURPXhWqRFOMboqDJzyLMyKUpDnyQPS0OenVmHMn/uys3Lw==",\n      "license": "MIT",\n      "peer": true,'
+      --replace-fail $'"node_modules/@earendil-works/pi-coding-agent/node_modules/@earendil-works/pi-agent-core": {\n      "version": "0.80.3",\n      "resolved": "https://registry.npmjs.org/@earendil-works/pi-agent-core/-/pi-agent-core-0.80.3.tgz",\n      "license": "MIT",\n      "peer": true,' \
+                     $'"node_modules/@earendil-works/pi-coding-agent/node_modules/@earendil-works/pi-agent-core": {\n      "version": "0.80.3",\n      "resolved": "https://registry.npmjs.org/@earendil-works/pi-agent-core/-/pi-agent-core-0.80.3.tgz",\n      "integrity": "sha512-3qw0/GeRQBU/nlGjDe5Yb7ePKTmoxefx2YxyKMFAviFUMXpFexBG/hS7mBtwFahFvzrrTPPoRT6sFIDjwoDWPQ==",\n      "license": "MIT",\n      "peer": true,' \
+      --replace-fail $'"node_modules/@earendil-works/pi-coding-agent/node_modules/@earendil-works/pi-ai": {\n      "version": "0.80.3",\n      "resolved": "https://registry.npmjs.org/@earendil-works/pi-ai/-/pi-ai-0.80.3.tgz",\n      "license": "MIT",\n      "peer": true,' \
+                     $'"node_modules/@earendil-works/pi-coding-agent/node_modules/@earendil-works/pi-ai": {\n      "version": "0.80.3",\n      "resolved": "https://registry.npmjs.org/@earendil-works/pi-ai/-/pi-ai-0.80.3.tgz",\n      "integrity": "sha512-jPZLMeGL5kkMSEAwAklfXTMHqZvfhsJtCCpKGIr5Duk7mc0n4skjB1dugk7y0z3z8ZHIUCmPAWHdyDqgUz5vdA==",\n      "license": "MIT",\n      "peer": true,' \
+      --replace-fail $'"node_modules/@earendil-works/pi-coding-agent/node_modules/@earendil-works/pi-tui": {\n      "version": "0.80.3",\n      "resolved": "https://registry.npmjs.org/@earendil-works/pi-tui/-/pi-tui-0.80.3.tgz",\n      "license": "MIT",\n      "peer": true,' \
+                     $'"node_modules/@earendil-works/pi-coding-agent/node_modules/@earendil-works/pi-tui": {\n      "version": "0.80.3",\n      "resolved": "https://registry.npmjs.org/@earendil-works/pi-tui/-/pi-tui-0.80.3.tgz",\n      "integrity": "sha512-2BJI6qwRQfnM0Q7seL1+SbacU/jRRjBnN7Hu3n9BjAn7/s5FaBNnvdD1qBQYRsFTHfjqMaDsjYqanPyqwXj99w==",\n      "license": "MIT",\n      "peer": true,'
   '';
 
   npmDepsFetcherVersion = 2;
-  npmDepsHash = "sha256-6VwCB5JdQK0BAV7kFJgk3NpSKAsCc1ozmZ7GXP0OE4o=";
+  npmDepsHash = "sha256-zYk00hhMcJeOFbv7qwzYMcHQ18CpTRuKwPiZHefoQZU=";
 
   prunePaths = [
     ".github"
