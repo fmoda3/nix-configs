e23d0b5c8d8269867509684126e3f4c8c49b2d4e
Frank Moda <fmoda3@mac.com>
2026-07-28 21:01:40 -0400

    Update opencode

--- pkgs/opencode/default.nix
+++ pkgs/opencode/default.nix
@@ -143,6 +143,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
 
     install -Dm644 config.json $out/share/opencode/config.json
     install -Dm644 tui.json $out/share/opencode/tui.json
+    install -Dm644 ../web/public/theme.json $out/share/opencode/theme.json
 
     runHook postInstall
   '';
@@ -167,6 +168,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
   passthru = {
     jsonschema = {
       config = "${placeholder "out"}/share/opencode/config.json";
+      theme = "${finalAttrs.finalPackage}/share/opencode/theme.json";
       tui = "${placeholder "out"}/share/opencode/tui.json";
     };
     updateScript = nix-update-script {
