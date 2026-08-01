e22c0d2b08439836f776b018891e4c838de06775
Frank Moda <fmoda3@mac.com>
2026-08-01 19:21:28 -0400

    Update codex build

--- pkgs/codex/librusty_v8.nix
+++ pkgs/codex/librusty_v8.nix
@@ -1,10 +1,11 @@
 # auto-generated file -- DO NOT EDIT!
 { fetchLibrustyV8 }:
 fetchLibrustyV8 {
-  version = "146.4.0";
+  version = "149.2.0";
   shas = {
-    x86_64-linux = "sha256-5ktNmeSuKTouhGJEqJuAF4uhA4LBP7WRwfppaPUpEVM=";
-    aarch64-linux = "sha256-2/FlsHyBvbBUvARrQ9I+afz3vMGkwbW0d2mDpxBi7Ng=";
-    aarch64-darwin = "sha256-v+LJvjKlbChUbw+WWCXuaPv2BkBfMQzE4XtEilaM+Yo=";
+    x86_64-linux = "sha256-iu2YY323533Iv7i7R1nsW95HLQv3lD9Y4OYqNQlFxVk=";
+    aarch64-linux = "sha256-+XdRJ8pk3MSjZi0BpSGizvuluY+DOUOog9hHc7Kv88U=";
+    x86_64-darwin = "sha256-eUlAo4o/ZrfvUqXwA8awlPdDrQQKZK+z082frUlADwc=";
+    aarch64-darwin = "sha256-+rsuyNO6Wm3qY9uaNalg3FypheujLzQrm6Sqocc0sv4=";
   };
 }
