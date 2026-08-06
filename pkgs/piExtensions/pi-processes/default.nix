390279f1c7a29df741e9ec3125c0cc65dedc3143
Frank Moda <fmoda3@mac.com>
2026-08-05 21:07:54 -0400

    Update pi-processes

--- pkgs/piExtensions/pi-processes/default.nix
+++ pkgs/piExtensions/pi-processes/default.nix
@@ -2,32 +2,32 @@
 , fetchFromGitHub
 , fetchPnpmDeps
 , nodejs
-, pnpm_10
+, pnpm_11
 , pnpmConfigHook
 }:
 
 buildPiExtension rec {
   pname = "pi-processes";
-  version = "2026-07-28";
+  version = "2026-08-05";
 
   src = fetchFromGitHub {
     owner = "aliou";
     repo = "pi-processes";
-    rev = "42d58914e9c22371cb080d55c94661db7f93c267";
-    sha256 = "sha256-KR2TJBSvGHk4/C5yo9vTQGa6SJRT5fRRIOArgHqmBLA=";
+    rev = "1ba2a4d8849dd43744712f515ef8e8a003bebbc8";
+    sha256 = "sha256-VOlf2skBi9xwc9eLDxErmTZuPL+tnuKhZdXV6tfAeQ4=";
   };
 
   pnpmDeps = fetchPnpmDeps {
     inherit pname version src;
-    fetcherVersion = 3;
-    hash = "sha256-5aQmg49vi+5Us56xoBaj+Jug4eVAw970UfegdQuIN4I=";
-    pnpm = pnpm_10;
+    fetcherVersion = 4;
+    hash = "sha256-EFUTuok2ji5W0cX7K9DDXRLFemQ6A7GTh9lVM8s0RqQ=";
+    pnpm = pnpm_11;
   };
 
   nativeBuildInputs = [
     nodejs
     pnpmConfigHook
-    pnpm_10
+    pnpm_11
   ];
 
   env.npm_config_manage_package_manager_versions = "false";
