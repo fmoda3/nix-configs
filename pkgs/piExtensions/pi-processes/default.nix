4d0a9314c419af49c855bf586aebdc2dc5a41267
Frank Moda <fmoda3@mac.com>
2026-08-08 13:21:18 -0400

    Update pi-processes

--- pkgs/piExtensions/pi-processes/default.nix
+++ pkgs/piExtensions/pi-processes/default.nix
@@ -8,19 +8,19 @@
 
 buildPiExtension rec {
   pname = "pi-processes";
-  version = "2026-08-07";
+  version = "2026-08-08";
 
   src = fetchFromGitHub {
     owner = "aliou";
     repo = "pi-processes";
-    rev = "c7f60f64b377f24f0b89890f66ab50642387ecc0";
-    sha256 = "sha256-xfy3ViScjUroeW7gaFgxjrsqcZnwwOIFsvRpl5YEdmk=";
+    rev = "71472f163769b19c144f6a16d492323c5f04c64e";
+    sha256 = "sha256-gW77wZyEUB00/PsoeOoGVNA3XsgviWskC6mJzTg+rsI=";
   };
 
   pnpmDeps = fetchPnpmDeps {
     inherit pname version src;
     fetcherVersion = 4;
-    hash = "sha256-VJTpexSfro+LgMDB3Ddqgs96cx7Yal5pYRzMyPyGqMs=";
+    hash = "sha256-TEAOhrJ2iDlH+0c16mN5TbJwk1XVl2ucL2716qbK4s4=";
     pnpm = pnpm_11;
   };
 
