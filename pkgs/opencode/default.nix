67c27da50261b8b1fa58395104b8f6b077ba7ae9
Frank Moda <fmoda3@mac.com>
2026-08-24 11:00:23 -0400

    Update opencode

--- pkgs/opencode/default.nix
+++ pkgs/opencode/default.nix
@@ -15,7 +15,7 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "opencode";
-  version = "1.18.21";
+  version = "1.18.22";
 
   __structuredAttrs = true;
   strictDeps = true;
@@ -24,7 +24,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     owner = "anomalyco";
     repo = "opencode";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-WKG/lts+wzDjYJ5pOZ0X4Kb0rJ1TzYQzQgjyQBY+bxs=";
+    hash = "sha256-EvhjSETkOL4s4PlUB2zvALU4eD2iCHZ3AFh+3ro95dw=";
   };
 
   node_modules = stdenvNoCC.mkDerivation {
@@ -77,7 +77,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     # NOTE: Required else we get errors that our fixed-output derivation references store paths
     dontFixup = true;
 
-    outputHash = "sha256-dGASaxZnxzJZY1PuDeqQCnYgMm2gEf5HZQsWOnt2JaU=";
+    outputHash = "sha256-9LKC35HS8rH/Zx3jm0hlCeg4AAEuWIhGtrmY3IU+MCY=";
     outputHashAlgo = "sha256";
     outputHashMode = "recursive";
   };
