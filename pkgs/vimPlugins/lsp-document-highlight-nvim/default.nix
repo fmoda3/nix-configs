fbcb4a09b4ae0812a7e117d9d71462e7acb0f4b3
Frank Moda <fmoda3@mac.com>
2026-09-13 09:44:47 -0400

    Update lsp-document-highlight-nvim

--- pkgs/vimPlugins/lsp-document-highlight-nvim/default.nix
+++ pkgs/vimPlugins/lsp-document-highlight-nvim/default.nix
@@ -3,12 +3,12 @@
 }:
 vimUtils.buildVimPlugin {
   pname = "lsp-document-highlight-nvim";
-  version = "2026-04-27";
+  version = "2026-09-12";
   src = fetchFromGitHub {
     owner = "akioweh";
     repo = "lsp-document-highlight.nvim";
-    rev = "a6bf150e994aa56b8aecf0b242ca757c7ce7de1f";
-    sha256 = "sha256-L41qTaJ4NKJ5TEEFB/6uOUQq1CB++oVgRiDuR7qoUo0=";
+    rev = "2097274ed9339beb36bb37cec3fe46890a970a2f";
+    sha256 = "sha256-KjwbH5sMLZfDmKpgEgIowUu1PqfZkNbRH4Ci3B03oyY=";
   };
   meta.homepage = "https://github.com/akioweh/lsp-document-highlight.nvim/";
 }
