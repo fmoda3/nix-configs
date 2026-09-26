11014e23eeadbd0ee5d8fd13d597cb1d216fb316
Frank Moda <fmoda3@mac.com>
2026-09-26 09:32:27 -0400

    Update pi-web-access

--- pkgs/piExtensions/pi-web-access/default.nix
+++ pkgs/piExtensions/pi-web-access/default.nix
@@ -4,13 +4,13 @@
 
 buildPiExtension {
   pname = "pi-web-access";
-  version = "2026-09-23";
+  version = "2026-09-26";
 
   src = fetchFromGitHub {
     owner = "nicobailon";
     repo = "pi-web-access";
-    rev = "610a52033f1e9705c0023ff9e0fac399319310a3";
-    sha256 = "sha256-ykR2slh8MkxxbP660h0rvk2Y7SaKv+Cw/lJC21JqGW8=";
+    rev = "fe4b7719ccd9b7e44526850259b2264c19ba8899";
+    sha256 = "sha256-X99magpK0lG4rJD1qvUh14Fz9eNecBU1WTWXtFIYk8o=";
   };
 
   postPatch = ''
